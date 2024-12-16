e8a08a292c4bd02e57bb95bdd7f02e65f43bc0e3
Bandi Sravani <bandisravani007@gmail.com>
2024-12-16 12:38:35 +0530

    Demo

---
+++ demo-FOR-JPA/src/test/java/com/example/demo/FOR/JPA/DemoForJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.FOR.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
